3f2856a30fd1bf91326520410f87185f9207c7a7
Venkateswara VP <venkateswara.vp@gmail.com>
2023-12-19 21:44:56 -0500

    first commit

---
+++ src/test/java/io/vpv/springtestcontainer/SpringTestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.vpv.springtestcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestcontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
